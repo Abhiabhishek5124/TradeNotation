7106e893e69288b403ec81c949e6277c1c40498e
Abhiabhishek5124 <78491711+Abhiabhishek5124@users.noreply.github.com>
2023-10-06 06:50:35 -0400

    updated

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db_url: "mongodb+srv://abhishekshrestha5125:WJRdpXh0sUIfA4yg@clustor1a.mrjuys9.mongodb.net/?retryWrites=true&w=majority",
+    db_url: "mongodb+srv://abhishekshrestha5125:VC1OZn3RmsTPXoC5@clustor1a.mrjuys9.mongodb.net/?retryWrites=true&w=majority",
     PORT: 3000
 };
